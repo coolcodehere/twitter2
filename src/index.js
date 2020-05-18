82c811558441fdb13586903854749a62e2065a29
coolcodehere <Bushfries@gmail.com>
2020-05-17 23:24:31 -0500

    Start and finish can be placed and also the board clears properlty

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const grid = document.getElementById("grid")
 const gridSize = 10
 grid.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
 grid.style.gridTemplatRows = `repeat(${gridSize}, auto)`;
-var start
-var finish
+let start
+let startCoordinatePair = {x:0, y:0}
+let finish 
+let finishCoordinatePair = {x:0, y:0};
 let brush
-var adjacencyMatrix = new Array(gridSize)
+let adjacencyMatrix = new Array(gridSize)
 
 const changeBrushSelectionListener = document.querySelectorAll('input[name="brush"]').forEach(function (el) {
   el.addEventListener("change",  function () {
@@ -14,18 +16,35 @@ const changeBrushSelectionListener = document.querySelectorAll('input[name="brus
 })
 
 const startClickListener = document.getElementById("start").addEventListener("click", function() {
+  eraseBoard(); 
+
   let path;
-  path = bfs({x:1, y:0},{x:9, y:4}).then((x) => {
-    var i = 1;
-    while (i < x.length) {
+  path = bfs(startCoordinatePair, finishCoordinatePair).then((x) => {
+    for (let i = 1; i < x.length; i++) {
       moveElement(adjacencyMatrix[x[i - 1].y][x[i-1].x], adjacencyMatrix[x[i].y][x[i].x])
-      i++
     }
   })
+  start.backgroundColor = ""
 })
+
+const clearClickListener = document.getElementById("clear").addEventListener("click", function() {
+  eraseBoard();
+});
 
 function getPair(x, y) {
   return {x:x, y:y}
+}
+
+function eraseBoard() {
+  console.log("Board Erased")
+  for (let y = 0; y < gridSize; y++) {
+    for (let x = 0; x < gridSize; x++) {
+      if (start !== adjacencyMatrix[y][x] && finish !== adjacencyMatrix[y][x]) {
+        adjacencyMatrix[y][x].style.backgroundImage = "none"
+        adjacencyMatrix[y][x].style.backgroundColor = "white"
+      }
+    }
+  }
 }
 
 function equals(p1, p2) {
@@ -73,15 +92,12 @@ function sleep(ms) {
 }
 
 async function bfs(startCoordinatePair, endCoordinatePair) {
-  let colors = ["red", "green", "blue", "orange", "magenta"]
-  let c = 0;
   let q = []
   let visited = {}
   let parentMap = {}
   q.push(startCoordinatePair)
 
   while (q.length != 0) {
-    c = (c + 1) % colors.length;
     await sleep(2);
     currPair = q.shift()
 
@@ -103,7 +119,11 @@ async function bfs(startCoordinatePair, endCoordinatePair) {
       if (isValidCoord(pairs[i]) && visited[computeKey(pairs[i])] === undefined) {
         console.log(pairs[i], !visited[computeKey(pairs[i])])
         visited[computeKey(pairs[i])] = true
-        adjacencyMatrix[pairs[i].y][pairs[i].x].style.backgroundColor = colors[c];
+
+        if (!equals(pairs[i], startCoordinatePair) && !equals(pairs[i], finishCoordinatePair)) {
+          adjacencyMatrix[pairs[i].y][pairs[i].x].style.backgroundColor = "green";
+        }
+          
         q.push(pairs[i])
         parentMap[computeKey(pairs[i])] = currPair
       }
@@ -112,7 +132,6 @@ async function bfs(startCoordinatePair, endCoordinatePair) {
 }
 
 function moveElement(fromElement, toElement) {
-  fromElement.style.backgroundColor = "white"
   toElement.style.backgroundImage = 'url(../src/dude.bmp)'
 }
 
@@ -136,6 +155,7 @@ for (let y = 0; y < gridSize; y++) {
           if (start) {
             start.style.backgroundColor = "white"
           }
+          startCoordinatePair = {x:x, y:y}
           start = gridElement
           gridElement.style.backgroundColor = "blue"
           gridElement.setAttribute("state", "start");
@@ -145,6 +165,7 @@ for (let y = 0; y < gridSize; y++) {
           if (finish) {
             finish.style.backgroundColor = "white"
           }
+          finishCoordinatePair = {x:x, y:y}
           finish = gridElement
           gridElement.style.backgroundColor = 'red'
           gridElement.setAttribute("state", "finish");
